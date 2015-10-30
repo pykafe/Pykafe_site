f55e5e700cb33b555c17f56b30ce81b288ca77cc
Nando <nando22alia@gmail.com>
2015-10-30 15:21:32 +0900

    Created login implementation

--- pykafe/members/urls.py
+++ pykafe/members/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from members import views
 
 urlpatterns = patterns('',
+    url(r'^login/$', views.Login.as_view(), name='login'),
     url(r'^$', views.Home.as_view(), name='home'), 
 )
--- pykafe/members/views.py
+++ pykafe/members/views.py
@@ -1,5 +1,29 @@
+from django.http import HttpResponseRedirect
+from django.conf import settings
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
 
 
 class Home(TemplateView):
     template_name = 'members/index.html'
+
+class Login(TemplateView):
+	template_name = 'members/login.html'
+	
+	def post(self, request, *agrs, **kwarg):
+	
+		context = self.get_context_data()
+	
+		username = request.POST['username']
+		password = request.POST['password']
+		
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			response = HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+		else:
+			context['login_filed'] = True
+			response = super(TemplateView, self).render_to_response(context)
+
+		return response
+		
