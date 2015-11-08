28937390483724240c20c765c129049c7df4f493
Joanico <joanicobarros@gmail.com>
2015-11-08 21:06:56 +0900

    created logout template

--- pykafe/members/urls.py
+++ pykafe/members/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, url
 
 from members import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.Home.as_view(), name='home'),
     url(r'^login/$', views.Login.as_view(), name='login'),
-    url(r'^$', views.Home.as_view(), name='home'), 
+    url(r'^logout/$', views.Logout.as_view(), name='logout'),
 )
--- pykafe/members/views.py
+++ pykafe/members/views.py
@@ -9,22 +9,25 @@ class Home(TemplateView):
 
 
 class Login(TemplateView):
-	template_name = 'members/login.html'
+    template_name = 'members/login.html'
 
-	def post(self, request, *agrs, **kwargs):
+    def post(self, request, *agrs, **kwargs):
 
-		context = self.get_context_data()
+        context = self.get_context_data()
 
-		username = request.POST['username']
-		password = request.POST['password']
+        username = request.POST['username']
+        password = request.POST['password']
 
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			login(request, user)
-			response = HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
-		else:
-			context['login_failed'] = True
-			response = super(TemplateView, self).render_to_response(context)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+        else:
+            context['login_failed'] = True
+            response = super(TemplateView, self).render_to_response(context)
 
-		return response
+        return response
 
+
+class Logout(TemplateView):
+    template_name = 'members/logout.html'
