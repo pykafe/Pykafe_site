d0d1ce4e48c649e4a122cc48531575b2a8a8a06c
mariano <dedeus_mariano@yahoo.com>
2015-11-08 14:42:59 +0900

    added boostrap on login

--- pykafe/members/views.py
+++ pykafe/members/views.py
@@ -7,16 +7,17 @@ from django.contrib.auth import authenticate, login
 class Home(TemplateView):
     template_name = 'members/index.html'
 
+
 class Login(TemplateView):
 	template_name = 'members/login.html'
-	
-	def post(self, request, *agrs, **kwarg):
-	
+
+	def post(self, request, *agrs, **kwargs):
+
 		context = self.get_context_data()
-	
+
 		username = request.POST['username']
 		password = request.POST['password']
-		
+
 		user = authenticate(username=username, password=password)
 		if user is not None:
 			login(request, user)
@@ -26,4 +27,4 @@ class Login(TemplateView):
 			response = super(TemplateView, self).render_to_response(context)
 
 		return response
-		
+
